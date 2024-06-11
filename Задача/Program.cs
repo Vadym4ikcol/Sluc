ec841bc5156fcbf95d9e0761b935bd0ff39cd10c
Vadym4ikcol <vimefredi@gmail.com>
2024-06-11 23:18:17 +1000

    Add files via upload

---
+++ Задача/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 101);
+            Console.WriteLine($"Случайное число: {randomNumber}");
+        }
+    }
+}
